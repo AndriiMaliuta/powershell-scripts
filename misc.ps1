bc572e80af907c74a0e0099abb4a87e549df5d14
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2021-12-31 15:03:27 +0200

    Add files via upload

---
+++ misc.ps1
@@ -0,0 +1,7 @@
+ Get-Item -Path Registry::HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\office\16.0\common\signin |>>   Select-Object -ExpandProperty Value
+ Get-ItemProperty -Path . 
+ Set-Item -Path Registry::HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\office\16.0\common\signin |>>   Select-Object -ExpandProperty Property
+  Get-Item -Path Registry::Computer\HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\office\16.0\common\signin |
+>>   Select-Object -ExpandProperty Property
+Get-Job -State Started
+Get-Job -Name "ja*"
