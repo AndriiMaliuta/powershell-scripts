3a3d7c5efeacb1f6466f745673e6e9e21ac137a8
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-01-29 09:47:49 -0800

    Update misc.ps1

--- misc.ps1
+++ misc.ps1
@@ -1,5 +1,6 @@
  Get-Item -Path Registry::HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\office\16.0\common\signin |>>   Select-Object -ExpandProperty Value
- Get-ItemProperty -Path . 
+ Get-ItemProperty -Path .
+ Set-Item -Path env:JAVA_HOME -Value "C:\Users\andrii\java"
  Set-Item -Path Registry::HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\office\16.0\common\signin |>>   Select-Object -ExpandProperty Property
   Get-Item -Path Registry::Computer\HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\office\16.0\common\signin |
 >>   Select-Object -ExpandProperty Property
