3f64e54248aeaefe230967c7bbcc4a4b3f45df17
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-01-30 02:06:53 -0800

    Create processes.ps1

---
+++ processes.ps1
@@ -0,0 +1,9 @@
+#Get-Process
+#   [[-Name] <String[]>]
+#   [-Module]
+#   [-FileVersionInfo]
+#   [<CommonParameters>]
+
+Get-Process winword, explorer | Format-List *
+
+Get-Process | Where-Object {$_.WorkingSet -gt 20000000}
