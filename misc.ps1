ae72dd4df1cc7b3f1a71fd385ee44bd266802d9f
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-01-30 00:39:00 -0800

    env

--- misc.ps1
+++
@@ -1,8 +0,0 @@
- Get-Item -Path Registry::HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\office\16.0\common\signin |>>   Select-Object -ExpandProperty Value
- Get-ItemProperty -Path .
- Set-Item -Path env:JAVA_HOME -Value "C:\Users\andrii\java"
- Set-Item -Path Registry::HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\office\16.0\common\signin |>>   Select-Object -ExpandProperty Property
-  Get-Item -Path Registry::Computer\HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\office\16.0\common\signin |
->>   Select-Object -ExpandProperty Property
-Get-Job -State Started
-Get-Job -Name "ja*"
