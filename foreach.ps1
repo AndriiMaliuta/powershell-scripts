dca808daff1bd0fac78779a24a82dd3d997d3f72
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-03-23 04:05:41 -0700

    Create foreach.ps1

---
+++ foreach.ps1
@@ -0,0 +1,35 @@
+foreach ($file in { Get-Item * })
+{
+    # When find succeeds, the loop breaks
+    Rename-Item $file 
+}
+
+Get-ChildItem -Path *.groovy |
+  Where-Object {$_.length -gt 10000} |
+    Sort-Object -Property length |
+      Format-Table -Property name, length
+
+# Wrapping with a pipe at the beginning of a line (no backtick required)
+Get-Process | Where-Object CPU | Where-Object Path
+    | Get-Item | Where-Object FullName -match "AppData"
+    | Sort-Object FullName -Unique
+
+# Wrapping with a pipe on a line by itself
+Get-Process | Where-Object CPU | Where-Object Path
+    |
+    Get-Item | Where-Object FullName -match "AppData"
+    |
+    Sort-Object FullName -Unique
+
+foreach ($file in Get-ChildItem)
+{
+  Write-Host $file
+}
+
+foreach ($file in Get-ChildItem)
+{
+  if ($file.length -gt 100KB)
+  {
+    Write-Host $file
+  }
+}    
